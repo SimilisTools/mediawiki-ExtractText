6ac3697f1103d9e2714f41c506f5b3c138f25a03
Toni Hermoso Pulido <toniher@cau.cat>
2014-10-19 22:59:26 +0200

    update extractText

--- ExtractText.php
+++ ExtractText.php
@@ -1,68 +1,67 @@
 <?php
 
 
-
 if ( !defined( 'MEDIAWIKI' ) ) {
-        die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
-$wgExtensionFunctions[] = 'wfSetupExtractText';
-$wgExtensionCredits['parserhook'][] = array(
-        'path' => __FILE__,
-        'name' => 'ExtractText',
-        'version' => '0.1',
-        'url' => 'https://www.mediawiki.org/wiki/Extension:ExtractText',
-        'author' => array( 'Toniher' ),
-        'descriptionmsg' => 'Extract text',
+$GLOBALS['wgExtensionCredits']['parserhook'][] = array(
+	'path' => __FILE__,
+	'name' => 'ExtractText',
+	'version' => '0.1',
+	'url' => 'https://github.com/SimilisTools/mediawiki-ExtractText',
+	'author' => array( 'Toniher' ),
+	'descriptionmsg' => 'extracttext-desc',
 );
 
-$wgAutoloadClasses['ExtExtractText'] = dirname(__FILE__).'/ExtractText_body.php';
-$wgExtensionMessagesFiles['ExtractText'] = dirname( __FILE__ ) . '/ExtractText.i18n.php';
-$wgExtensionMessagesFiles['ExtractTextMagic'] = dirname( __FILE__ ) . '/ExtractText.i18n.magic.php';
+$GLOBALS['wgAutoloadClasses']['ExtExtractText'] = __DIR__.'/ExtractText_body.php';
+$GLOBALS['wgMessagesDirs']['ExtractText'] = __DIR__ . '/i18n';
+$GLOBALS['wgExtensionMessagesFiles']['ExtractText'] = __DIR__ . '/ExtractText.i18n.php';
+$GLOBALS['wgExtensionMessagesFiles']['ExtractTextMagic'] = __DIR__ . '/ExtractText.i18n.magic.php';
+
+$GLOBALS['wgExtensionFunctions'][] = 'wfSetupExtractText';
 
 function wfSetupExtractText() {
 
 	global $wgETHookStub, $wgHooks;
 
-        $wgETHookStub = new ExtractText_HookStub;
+	$wgETHookStub = new ExtractText_HookStub;
 
-        $wgHooks['ParserFirstCallInit'][] = array( &$wgETHookStub, 'registerParser' );
-        $wgHooks['ParserClearState'][] = array( &$wgETHookStub, 'clearState' );
+	$wgHooks['ParserFirstCallInit'][] = array( &$wgETHookStub, 'registerParser' );
+	$wgHooks['ParserClearState'][] = array( &$wgETHookStub, 'clearState' );
 
 }
 
 class ExtractText_HookStub {
-
+	
 	var $realObj;
-
-        function registerParser( &$parser ) {
+	
+		function registerParser( &$parser ) {
 		$parser->setFunctionHook( 'extracttext', array( &$this, 'extracttext' ), SFH_OBJECT_ARGS );
 		$parser->setFunctionHook( 'extractpagetext', array( &$this, 'extractpagetext' ), SFH_OBJECT_ARGS );
 		return true;
 	}
+	
+	/**
+	 * Defer ParserClearState
+	 */
+	function clearState( &$parser ) {
+		if ( !is_null( $this->realObj ) ) {
+				$this->realObj->clearState( $parser );
+		}
+		return true;
+	}
 
-        /**
-         * Defer ParserClearState
-         */
-        function clearState( &$parser ) {
-                if ( !is_null( $this->realObj ) ) {
-                        $this->realObj->clearState( $parser );
-                }
-                return true;
-        }
-
-        /**
-         * Pass through function call
-         */
-        function __call( $name, $args ) {
-                if ( is_null( $this->realObj ) ) {
-                        $this->realObj = new ExtExtractText;
-                        $this->realObj->clearState( $args[0] );
-                }
-                return call_user_func_array( array( $this->realObj, $name ), $args );
-        }
-
-
+	/**
+	 * Pass through function call
+	 */
+	function __call( $name, $args ) {
+		if ( is_null( $this->realObj ) ) {
+				$this->realObj = new ExtExtractText;
+				$this->realObj->clearState( $args[0] );
+		}
+		return call_user_func_array( array( $this->realObj, $name ), $args );
+	}
 
 }
 
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ mediawiki-ExtractText
 =====================
 
 Extension for processing text
+
+* {{#extracttext:wikitext|num of chars|extra text}}
+* {{#extractpagetext:pagename|num of chars|extra text}}
+
